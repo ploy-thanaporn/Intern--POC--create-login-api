65f2465948c8c211e55d89be09ea954fe9de9f79
ploy-thanaporn <tnpsp1501@gmail.com>
2022-07-18 16:28:39 +0700

    success condition

---
+++ src/components/Login.js
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import "./Login.css";
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorEmail, setErrorEmail] = useState("");
+  const [errorPassword, setErrorPassword] = useState("");
+
+  const [emailColor, setEmailColor] = useState("");
+  const [passwordColor, setPasswordColor] = useState("");
+
+  const validateForm = (e) => {
+    // ไม่ reset ค่า ในฟอร์ม
+    e.preventDefault();
+
+    if (email.includes("@")) {
+      setErrorEmail("");
+      setEmailColor("green");
+    } else {
+      setErrorEmail("Invalid email format");
+      setEmailColor("red");
+    }
+
+    let pwd = /^(?=.{10,})(?=.*[a-z])(?=.*[A-Z])(?=.*[@#S%^!&*+=]).*$/i;
+    if (pwd.test(password)) {
+      setErrorPassword("");
+      setPasswordColor("green");
+    } else {
+      setErrorPassword(
+        "have a minimum of 10 characters, allow only English characters, contain at least 1 lower and 1 upper, contain at least 1 special character and required validator"
+      );
+      setPasswordColor("red");
+    }
+  };
+  return (
+    <div className="container" onSubmit={validateForm}>
+      <form className="form">
+        <h2>Login</h2>
+        <div className="form-control">
+          <input
+            type="text"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            style={{ borderColor: emailColor }}
+          />
+          <small style={{ color: emailColor }}>{errorEmail}</small>
+        </div>
+        <div className="form-control">
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            style={{ borderColor: passwordColor }}
+          />
+          <small style={{ color: passwordColor }}>{errorPassword}</small>
+        </div>
+        <div className="btn">
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/components/dashbord.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Dashbord = () => {
+  return <div>Dashboard</div>;
+};
+
+export default Dashbord;
